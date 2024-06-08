426860ceb44cbd00e32cd48818e155224a01b3dc
= <=>
2024-06-08 11:31:17 +0000

    My portofolio

---
+++ portofolio.js
@@ -0,0 +1,11 @@
+
+const style_css = document.querySelectorAll('.content_competences aside:nth-child(5) div div:first-child ')
+style_css.forEach(element => {
+    element.classList.add('class_h5');
+});
+
+const style_css2 = document.querySelectorAll('.content_competences aside:nth-child(5) div div:last-child ')
+style_css2.forEach(element => {
+    element.classList.add('class_span');
+});
+
